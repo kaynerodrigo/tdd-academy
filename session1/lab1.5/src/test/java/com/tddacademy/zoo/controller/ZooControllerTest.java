9e7084cd1b22bec31201dcdae0db40de9e330403
Kayne Rodrigo <kayneuriel.rodrigo@gcash.com>
2025-07-23 19:53:01 +0800

    added contribution for Labs 0.5, Lab 1, and Lab 1.5
    Co-authored-by: Kayne Uriel Rodrigo <kayneuriel.rodrigo@gcash.com>
    Co-authored-by Jahren Hans Dayag <jarenhans.dayag@gcash.com>

--- session1/lab1.5/src/test/java/com/tddacademy/zoo/controller/ZooControllerTest.java
+++ session1/lab1.5/src/test/java/com/tddacademy/zoo/controller/ZooControllerTest.java
@@ -67,6 +67,6 @@ class ZooControllerTest {
         // 2. Add expectation for status().isNotFound()
 
         mockMvc.perform(get("/api/zoos/999"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound()); // checks if succeeds
     }
 } 
